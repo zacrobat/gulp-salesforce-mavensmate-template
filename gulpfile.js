af8b2ade54119647f8ce2fc7584fd28c77cbfa78
Zac Cobb <zcobb@salesforce.com>
2017-11-10 11:40:56 +1100

    Update file path for gulp transfer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('transfer', function() {
-  return gulp.src('../../mavensmate/the-current-project-you-are-working-on/src/aura/NAMESPACE_*/**')
+  return gulp.src('../../mavensmate/the-current-project-you-are-working-on/src/aura/**')
     .pipe(gulp.dest('./markup/'));
 });
