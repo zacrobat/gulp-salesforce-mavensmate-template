2764662ec964cd31235ad0befcceff23c3a41b8e
Zac Cobb <zcobb@salesforce.com>
2018-03-20 12:58:52 +1100

    Update gulpfile to transfer entire src folder to include apex classes plus remove offending comma from package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 
 gulp.task('transfer', function() {
-  return gulp.src('../../mavensmate/the-current-project-you-are-working-on/src/aura/**')
-    .pipe(gulp.dest('./markup/'));
+  return gulp.src('../../mavensmate/the-current-project-you-are-working-on/src/**')
+    .pipe(gulp.dest('./src/'));
 });
